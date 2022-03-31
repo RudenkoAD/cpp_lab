3b8677a6e7570717ea5c84c218cf3f4a3704500f
RudenkoAD <rudenkoAD@phystech.edu>
2022-03-31 09:21:24 +0300

    add lab5 task C

---
+++ classwork5/C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int counter(){
+    static int a = 0;
+    a++;
+    return a;
+}
+
+int main(){
+    std::cout<<counter();
+    std::cout<<counter();
+    std::cout<<counter();
+    std::cout<<counter();
+    std::cout<<counter();
+    std::cout<<counter();
+}
