974e1eefbfcbb941f17082c8c7e96b468f94e88f
RudenkoAD <rudenkoAD@phystech.edu>
2022-04-01 13:22:02 +0300

    amend 2-nd lab work

--- lab2/B.cpp
+++ lab2/B.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 
 float adjacent(float *psi, float *pdf, float dv, int size){
-    float sum = 0.0;
-    for (int step=2; step<size; step*=2) {
-        for (int i = 0; i < size; i += step) {
-            psi[i] * pdf[i] + psi[i + step / 2] * pdf[i + step / 2];
-        }
+    for (int i=0; i < size; i += 1) {
+        psi[i] = psi[i]*pdf[i];
     }
-    return sum;
+    for (int step=2; step<size; step*=2) {
+        int i=0;
+        for (; i + step/2 < size; i += step) {
+            psi[i] = psi[i] + psi[i + step / 2];
+        }
+        if (i<size) psi[i-step]+=psi[i]; //fixes odd amounts of elements, may be very bad.
+    }
+    return psi[0]*dv;
 } //calculates weighted average by summing adjacent elements repeatedly
 
 int main(){
